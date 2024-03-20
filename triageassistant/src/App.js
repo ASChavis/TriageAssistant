be15b087b684f03cc3bcb3d2873ddea900358d97
ASChavis <126298472+ASChavis@users.noreply.github.com>
2024-03-20 19:58:53 -0400

    Created React template
    Start of the initial page

---
+++ triageassistant/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Triage Assistant</h1>
+          
+        <p>Version 1</p>
+
+        <a>Start</a>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
